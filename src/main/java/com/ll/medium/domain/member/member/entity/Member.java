4f4f596bbbba1ca8bd06fb177bcd8d08044c370e
jkeum-dev <jon9eun@gmail.com>
2023-12-31 02:05:23 +0900

    admin

--- src/main/java/com/ll/medium/domain/member/member/entity/Member.java
+++ src/main/java/com/ll/medium/domain/member/member/entity/Member.java
@@ -5,8 +5,13 @@ import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -25,4 +30,16 @@ public class Member {
 	private LocalDateTime modifyDate;
 	private String username;
 	private String password;
+
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<GrantedAuthority> authorities = new ArrayList<>();
+
+		authorities.add(new SimpleGrantedAuthority("ROLE_MEMBER"));
+
+		if (List.of("system", "admin").contains(username)) {
+			authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+		}
+
+		return authorities;
+	}
 }
