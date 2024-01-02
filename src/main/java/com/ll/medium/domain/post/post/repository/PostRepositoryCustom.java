aaf4c23683aedc89711a266c649ae0bfc8345fe4
jkeum-dev <jon9eun@gmail.com>
2024-01-02 22:28:38 +0900

    blog 특정 회원의 글 list

--- src/main/java/com/ll/medium/domain/post/post/repository/PostRepositoryCustom.java
+++ src/main/java/com/ll/medium/domain/post/post/repository/PostRepositoryCustom.java
@@ -8,5 +8,5 @@ import org.springframework.data.domain.Pageable;
 public interface PostRepositoryCustom {
 	Page<Post> search(boolean isPublished, String kw, Pageable pageable);
 
-	Page<Post> search(Member author, String kw, Pageable pageable);
+	Page<Post> search(Member author, Boolean isPublished, String kw, Pageable pageable);
 }
