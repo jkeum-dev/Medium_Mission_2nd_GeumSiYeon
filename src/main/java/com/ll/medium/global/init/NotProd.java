9726f99401be4946b9c9bd56ee2073e53e6ac898
jkeum-dev <jon9eun@gmail.com>
2024-01-05 15:37:44 +0900

    충돌 해결하면서 잘못 고쳐진 코드 수정

--- src/main/java/com/ll/medium/global/init/NotProd.java
+++ src/main/java/com/ll/medium/global/init/NotProd.java
@@ -40,32 +40,31 @@ public class NotProd {
 
 	@Transactional
 	public void work1() {
-		Member memberUser1 = memberService.join("user1", "1234").getData();
-		Member memberUser2 = memberService.join("user2", "1234").getData();
-		Member memberUser3 = memberService.join("user3", "1234").getData();
+		Member memberUser1 = memberService.join("user1", "1234", true).getData();
+		Member memberUser2 = memberService.join("user2", "1234", true).getData();
+		Member memberUser3 = memberService.join("user3", "1234", true).getData();
 
-		Post post1 = postService.write(memberUser1, "제목 1", "내용 1", true);
-		Post post2 = postService.write(memberUser1, "제목 2", "내용 2", true);
-		Post post3 = postService.write(memberUser1, "제목 3", "내용 3", false);
-		Post post4 = postService.write(memberUser1, "제목 4", "내용 4", true);
+		Post post1 = postService.write(memberUser1, "제목 1", "내용 1", true, true);
+		Post post2 = postService.write(memberUser1, "제목 2", "내용 2", true, false);
+		Post post3 = postService.write(memberUser1, "제목 3", "내용 3", false, true);
+		Post post4 = postService.write(memberUser1, "제목 4", "내용 4", true, true);
 
-		Post post5 = postService.write(memberUser2, "제목 5", "내용 5", true);
-		Post post6 = postService.write(memberUser2, "제목 6", "내용 6", false);
+		Post post5 = postService.write(memberUser2, "제목 5", "내용 5", true, true);
+		Post post6 = postService.write(memberUser2, "제목 6", "내용 6", false, false);
 
 		postService.like(memberUser2, post1);
 		postService.like(memberUser3, post1);
-		postService.like(memberUser4, post1);
 
 		postService.like(memberUser2, post2);
 		postService.like(memberUser3, post2);
 
 		postService.like(memberUser2, post3);
 
-    IntStream.rangeClosed(51, 150).forEach(i -> {
-      postService.write(
-          memberService.join("user" + i, "1234", true).getData(),
-          "제목 " + i, "내용 " + i, true, true
-      );
-    });
+		IntStream.rangeClosed(51, 150).forEach(i -> {
+		  postService.write(
+			  memberService.join("user" + i, "1234", true).getData(),
+			  "제목 " + i, "내용 " + i, true, true
+		  );
+		});
 	}
 }
